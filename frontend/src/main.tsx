8fcad4ab0bc43af6a280eecface3d46f51cfe2aa
zhu-hong <zhxhyy@qq.com>
2023-04-26 20:21:42 +0800

    优化在wails中的启动逻辑

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,13 +7,6 @@ import '@/assets/style.css'
 
 import 'uno.css'
 
-export const inWails = window.wails != undefined
-console.log(`%cinWails: ${inWails}`, 'color:#f07c82;font-size:16px;')
-
-if(inWails) {
-  document.title = `${document.title} | wails`
-}
-
 createRoot(document.getElementById('goseph')!).render(
   <StrictMode>
     <App />
--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+/**
+ * @description 是否在wailsapp中
+ */
+declare const inWails: boolean;
+
 interface Window {
   wails: any;
 }
