d250eeb7ac428690ffbe204b44402dbb3e013bee
“郭国龙” <2630625174@qq.com>
2023-04-27 10:22:54 +0800

    修改移动端适配,增加暗色主题

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,23 +1,76 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, ChangeEvent, useRef } from "react";
+import { GetIP } from "@wailsjs/go/main/App";
+import axios from "axios";
 import "./Chat.scss";
 import { Button } from "antd";
 
 export default function Chat() {
-  const [isSetBox, setIsSetBox] = useState(false);
-  const [zdclass, setZdClass] = useState("zidan-img");
-  const setbox: any = useRef();
+  const [sboxClass, setSBoxClass] = useState("setting-box-none"); //设置弹出盒子是否显示
+  const [zdclass, setZdClass] = useState("zidan-img"); //子弹class
+  const [ip, setIP] = useState(""); //ip
+  const [file, setFile] = useState("未选择"); //选择的文件
+  const [mainClass, setMClass] = useState("chat-main"); //底部背景色
+  const [content, setContent] = useState("chat-content"); //聊天盒子背景色
+
+  // class主题切换
+  function themeChange() {
+    setMClass(mainClass === "chat-main" ? "chat-mian-black" : "chat-main");
+    setContent(
+      content === "chat-content" ? "chat-content-black" : "chat-content"
+    );
+    setSBoxClass(
+      sboxClass === "setting-box-block"
+        ? "setting-box-bblack"
+        : "setting-box-block"
+    );
+  }
+  // 改变子弹的className
   function changeClass() {
     setZdClass("zidan-img-move");
     setTimeout(() => {
       setZdClass("zidan-img");
     }, 300);
   }
+
+  async function onInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    const fm = new FormData();
+    fm.append("fileName", file.name);
+    fm.append("isFrag", file.name);
+    fm.append("file", file);
+
+    setFile(file.name);
+
+    if (window.wails === undefined) {
+      axios.post(`http://${file.type}:1122/api/v1/upload`, fm);
+    } else {
+      const curip = await GetIP();
+      console.log(curip);
+      axios.post(`http://${curip}:1122/api/v1/upload`, fm);
+    }
+  }
+
+  async function getIp() {
+    if (window.wails === undefined) return alert("not inWails");
+    const tip = await GetIP();
+    setIP(tip);
+  }
+
   return (
-    <div className="chat-main">
+    <div className={mainClass}>
       <div
         className="triggle-img"
         onClick={() => {
-          setIsSetBox(!isSetBox);
+          setSBoxClass(
+            sboxClass === "setting-box-block" ||
+              sboxClass === "setting-box-bblack"
+              ? "setting-box-none"
+              : sboxClass === "setting-box-none" &&
+                mainClass === "chat-mian-black"
+              ? "setting-box-bblack"
+              : "setting-box-block"
+          );
         }}
       ></div>
       <div className="joker-img"></div>
@@ -26,28 +79,35 @@ export default function Chat() {
       <div className="liwu-img"></div>
       <div className="huanglian-img"></div>
       <div className="lvlian-img"></div>
-      <div
-        ref={setbox}
-        className={isSetBox ? "setting-box-block" : "setting-box-none"}
-      >
+      <div className={sboxClass}>
         <div className="setting-box-close">
           <div
             onClick={() => {
-              setIsSetBox(false);
+              setSBoxClass("setting-box-none");
             }}
             className="img"
           ></div>
         </div>
-        <Button>setting</Button>
+        <Button onClick={themeChange}>切换主题色</Button>
       </div>
-      <div className="chat-content">
+      <div className={content}>
         {/* 飞机一直移动 */}
         <div className="plane-move1"></div>
         <div className="plane-move2"></div>
         <div className="plane-move3"></div>
         <div className="chat-dialog">
           {/* 聊天框 */}
-          <div className="chat-chat"></div>
+          <div className="chat-chat">
+            {/* <input
+              title="react"
+              type="file"
+              multiple
+              onChange={onInputChange}
+            /> */}
+            <span>选择的文件:{file}</span>
+            <span>{ip}</span>
+            <button onClick={getIp}>getip</button>
+          </div>
           {/* 底部飞机按钮,选择文件 */}
           <div className="bottom-btn">
             <div className="pre-btn"></div>
@@ -55,7 +115,15 @@ export default function Chat() {
             <div className={zdclass}></div>
             {/* 飞机 */}
             <div className="plane-box">
-              <div onClick={changeClass} className="plane1-img"></div>
+              <div onClick={changeClass} className="plane1-img">
+                <input
+                  title="react"
+                  type="file"
+                  multiple
+                  onChange={onInputChange}
+                  style={{ opacity: 0, position: "relative", bottom: "-50px" }}
+                />
+              </div>
             </div>
             <div className="next-btn"></div>
           </div>
